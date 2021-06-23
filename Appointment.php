6f29259b02d0dac3538a1691944a209062f77713
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-06-23 22:51:38 +0530

    Update Appointment.php

--- Appointment.php
+++ Appointment.php
@@ -5,6 +5,18 @@
   <link rel="stylesheet" type="text/css" href="Appointment.css">
 </head>
 <body>
+  <div class="layer">
+    <div class="headquate">
+       <img class="logo" src="https://cdn.iconscout.com/icon/free/png-256/pet-hospital-1131157.png">
+        <div class="quate">
+            <h1>Book You Slot And Save Your Time</h1>
+            <div class="topic">
+                <p>Pet Healthcare At your Demand !<br>Find a nearby doctor and book an appointment instanly.</p>
+              </div>
+              <img class="pet" src="https://gdurl.com/N2RX">
+        </div>
+    </div>
+   </div<
 <?php
 
 ?>
