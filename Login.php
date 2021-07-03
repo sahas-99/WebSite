a627bef9545bf1246097e19f3fff73a8457249b4
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-04 01:18:47 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,2 +1,17 @@
-<?
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
 ?>
