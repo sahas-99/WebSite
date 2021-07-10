a8561aef31a202b918a31d5a6c4ef8e4b6c838bb
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-10 20:22:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,25 @@
             </div>
                
       </div>
+	      <div class="box">
+          
+           <img src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif" class="avater">
+          <form action="login.php" method="post">
+      <h2>SIGN IN</h2>
+      <?php if (isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error']; ?></p>
+      <?php } ?>
+      <label>User Name</label>
+      <input type="text" name="uname" placeholder="User Name"><br>
+
+      <label>Password</label>
+      <input type="password" name="password" placeholder="Password"><br>
+
+      <button type="submit" value="">Sign in</button>
+          <a href="signup.php" class="ca">Create an account</a>
+     </form>
+</div>     
+     </div>
 	 </section>
      
 </body>
