f23fe856ba9b95c96d6013a8070efd820f0248f6
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-10 20:15:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 <html>
 <head>
 	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="new.css">
 	
 </head>
 <body>
+	<section>
+      <div class="circle"></div>
+  
+     <div class="container">
+          <div class="text">
+               <h1 class="logo">PET CITY</h1>
+               <h2>Pets are humanizing.They reminds us we have an obligaton and responsibility to preserve and nurture and care for all life....</h2>
+               <div class="img">
+                    <img src="https://cdn.pixabay.com/photo/2020/07/17/18/16/pets-5414987_960_720.png" class="photo">
+            </div>
+               
+      </div>
+	 </section>
      
 </body>
 </html>
