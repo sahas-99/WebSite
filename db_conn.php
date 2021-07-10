005af4047ae345de70c5b1f8a06048a148fba5f3
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-10 15:01:30 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,3 +1,13 @@
-<?
-php
+<?php
+$sname= "localhost";
+$unmae= "root";
+$password = "";
+
+$db_name = "test_db";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
 ?>
