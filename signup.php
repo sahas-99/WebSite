a25b02326bb4ac51df22a6b4d037bcbf030ebd17
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-12 11:55:10 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="new.css">
+</head>
+<body>
+     <section>
+          <div class="circle"></div>
+          <div class="container">
+          <div class="text">
+               <h1 class="logo">PET CITY</h1>
+               <h2>Pets are humanizing.They reminds us we have an obligaton and responsibility to preserve and nurture and care for all life..</h2>
+               <div class="img">
+                    <img src="https://www.clker.com/cliparts/6/2/g/v/I/d/pet-md.png" class="paw">
+               </div>
+          </div>
+</div>
+
+     <div class="box">
+           <img src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif" class="avater">
+     <form action="signup-check.php" method="post">
+     	<h2>SIGN UP</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+
+          <label>Name</label>
+          <?php if (isset($_GET['name'])) { ?>
+               <input type="text" 
+                      name="name" 
+                      placeholder="Name"
+                      value="<?php echo $_GET['name']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="name" 
+                      placeholder="Name"><br>
+          <?php }?>
+
+          <label>User Name</label>
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"><br>
+          <?php }?>
+
+
+     	<label>Password</label>
+     	<input type="password" 
+                 name="password" 
+                 placeholder="Password"><br>
+
+          <label>Confirm Password</label>
+          <input type="password" 
+                 name="re_password" 
+                 placeholder="Confirm Password"><br>
+
+     	<button type="submit">Sign Up</button>
+          <a href="index.php" class="ca">Already have an account?</a>
+     </form>
+</div>
+
+</section>
+</body>
+</html>
