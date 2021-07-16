ad22a3f380930262b98138f2be920900cfff56c8
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-16 23:59:32 +0530

    Update home.php

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <html>
 <head>
 	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="new.css">
 </head>
 <body>
      <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
