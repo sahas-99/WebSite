f3e2279f5610336853212340a734a6cecd1e25d2
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-16 16:55:04 +0530

    Update Appointment.php

--- Appointment.php
+++ Appointment.php
@@ -7,13 +7,13 @@
 <body>
   <div class="layer">
     <div class="headquate">
-       <img class="logo" src="https://cdn.iconscout.com/icon/free/png-256/pet-hospital-1131157.png">
+       <img class="logo" src="image/a-1.png">
         <div class="quate">
             <h1>Book You Slot And Save Your Time</h1>
             <div class="topic">
                 <p>Pet Healthcare At your Demand !<br>Find a nearby doctor and book an appointment instanly.</p>
               </div>
-              <img class="pet" src="https://gdurl.com/N2RX">
+              <img class="pet" src="image/a-3.png">
         </div>
     </div>
     <div class="appointment">
