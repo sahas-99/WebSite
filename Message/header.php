a28f9ac674f6c9579d2460f05fe87ebe078a5989
W.S.M. Silva <82355934+sahas-99@users.noreply.github.com>
2021-07-18 15:20:06 +0530

    Update header.php

--- Message/header.php
+++ Message/header.php
@@ -8,3 +8,4 @@
   <title>PetCity | Realtime Chat</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  </head>
