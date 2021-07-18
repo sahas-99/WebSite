03326916907268c70d06c44d38c598a48f132ebd
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-18 14:56:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -14,10 +14,10 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 	if (empty($uname)) {
-		header("Location: index.php?error=User Name is required");
+		header("Location: signin.php?error=User Name is required");
 	    exit();
 	}else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
+        header("Location: signin.php?error=Password is required");
 	    exit();
 	}else{
 		// hashing the password
@@ -37,17 +37,17 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             	header("Location: home.php");
 		        exit();
             }else{
-				header("Location: index.php?error=Incorect User name or password");
+				header("Location: signin.php?error=Incorect User name or password");
 		        exit();
 			}
 		}else{
-			header("Location: index.php?error=Incorect User name or password");
+			header("Location: signin.php?error=Incorect User name or password");
 	        exit();
 		}
 	}
 	
 	}else{
-	header("Location: index.php");
+	header("Location: signin.php");
 	exit();
 }
 	
