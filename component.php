ae2ce25b8123c41fd0b358d6b62b22a55151a378
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-20 18:12:52 +0530

    Update component.php

--- component.php
+++ component.php
@@ -1,2 +1,35 @@
 <?php
-?>
+function component($productname, $productprice, $productimg,$productid){
+    $element = "
+    
+    <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
+                <form action=\"index.php\" method=\"post\">
+                    <div class=\"card shadow my-5\">
+                        <div>
+                            <img src=\"$productimg\" alt=\"Image1\" class=\"img-fluid card-img-top\">
+                        </div>
+                        <div class=\"card-body\">
+                            <h5 class=\"card-title\">$productname</h5>
+                            <h6>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"far fa-star\"></i>
+                            </h6>
+                            
+                            <h5>
+                               
+                                <span class=\"price\">Rs.$productprice</span>
+                            </h5>
+
+                            <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">Add to Cart <i class=\"fas fa-shopping-cart\"></i></button>
+                            <input type='hidden' name='product_id' value='$productid'>
+                            
+                        </div>
+                    </div>
+                </form>
+            </div>
+    ";
+    echo $element;
+}
