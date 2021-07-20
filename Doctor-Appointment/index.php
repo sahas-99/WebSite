f4e051593982b268b90c8a04668a8d923c7782e0
W.S.M. Silva <82355934+sahas-99@users.noreply.github.com>
2021-07-20 16:03:45 +0530

    Update index.php

--- Doctor-Appointment/index.php
+++ Doctor-Appointment/index.php
@@ -95,6 +95,9 @@ alert('User already registered. Please try again');
         <!--Font Awesome (added because you use icons in your prepend/append)-->
         <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
         <link href="assets/css/material.css" rel="stylesheet">
+
+        <!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
     </head>
     <body>
         <!-- navigation -->
@@ -118,6 +121,7 @@ alert('User already registered. Please try again');
                         
 
                         <!-- <li><a href="adminlogin.php">Admin</a></li> -->
+                        <li><a href="Home.html"><i class="fas fa-home"></i></a></li>
                         <li><a href="#" data-toggle="modal" data-target="#myModal">Sign Up</a></li>
                    
                         <li>
@@ -249,39 +253,20 @@ alert('User already registered. Please try again');
                                                 <select name="year" class = "form-control input-lg" required>
                                                     <option value="">Year</option>
                                                     
-                                                    <option value="1981">1981</option>
-                                                    <option value="1982">1982</option>
-                                                    <option value="1983">1983</option>
-                                                    <option value="1984">1984</option>
-                                                    <option value="1985">1985</option>
-                                                    <option value="1986">1986</option>
-                                                    <option value="1987">1987</option>
-                                                    <option value="1988">1988</option>
-                                                    <option value="1989">1989</option>
-                                                    <option value="1990">1990</option>
-                                                    <option value="1991">1991</option>
-                                                    <option value="1992">1992</option>
-                                                    <option value="1993">1993</option>
-                                                    <option value="1994">1994</option>
-                                                    <option value="1995">1995</option>
-                                                    <option value="1996">1996</option>
-                                                    <option value="1997">1997</option>
-                                                    <option value="1998">1998</option>
-                                                    <option value="1999">1999</option>
-                                                    <option value="2000">2000</option>
-                                                    <option value="2001">2001</option>
-                                                    <option value="2002">2002</option>
-                                                    <option value="2003">2003</option>
-                                                    <option value="2004">2004</option>
-                                                    <option value="2005">2005</option>
-                                                    <option value="2006">2006</option>
-                                                    <option value="2007">2007</option>
-                                                    <option value="2008">2008</option>
-                                                    <option value="2009">2009</option>
                                                     <option value="2010">2010</option>
                                                     <option value="2011">2011</option>
                                                     <option value="2012">2012</option>
                                                     <option value="2013">2013</option>
+                                                    <option value="2014">2014</option>
+                                                    <option value="2015">2015</option>
+                                                    <option value="2016">2016</option>
+                                                    <option value="2017">2017</option>
+                                                    <option value="2018">2018</option>
+                                                    <option value="2019">2019</option>
+                                                    <option value="2020">2020</option>
+                                                    <option value="2021">2021</option>
+                                                    <option value="2022">2022</option>
+                                                    
                                                 </select>
                                             </div>
                                         </div>
