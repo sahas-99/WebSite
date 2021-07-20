efdfec78ae70766b428c5c2c8eeb4085aedcc63d
R.G.S.Madubhashani <72640031+samindi98@users.noreply.github.com>
2021-07-20 18:14:43 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,2 +1,59 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="Web.css">
+</head>
+<body>
+    <header id="header">
+     <nav style=" color: #fff;"  class="navbar navbar-expand-lg navbar-dark bg-info">
+       
+        <!-- Navigation -->
+    <nav class="nav">
+      <div class="navigation container">
+        <a href="Home.html"><i class="fas fa-home"></i></a>
+        <div class="logo">
+
+          <h1>Pet Shop</h1>
+        </div>
+
+        <div class="menu">
+          <div class="top-nav">
+            <div class="logo">
+              <h1>Pet Shop</h1>
+            </div>
+            <div class="close">
+              <i class="bx bx-x"></i>
+            </div>
+          </div>
+          <ul class="nav-list">
+            <li class="nav-item">
+              <a href="#home" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="Dog.html" class="nav-link">Dog</a>
+            </li>
+            <li class="nav-item">
+              <a href="Cat.html" class="nav-link">Cat</a>
+            </li>
+            <li class="nav-item">
+              <a href="Bird.html" class="nav-link">Bird</a>
+            </li>
+            <li class="nav-item">
+              <a href="Fish.html" class="nav-link">Fish</a>
+            </li>
+            <li class="nav-item">
+                <a href="Small Animals.html" class="nav-link">Others</a>
+              </li>
+            <li class="nav-item">
+              
+            </li>
+          </ul>
+
+        </div>
+        
+        
+        
+        </body>
+      </html
